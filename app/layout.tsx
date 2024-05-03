f2ed2558e67038147dd2b8be1f60a7b19754413a
YoeCK-sys <114443942+YoeCK-sys@users.noreply.github.com>
2024-05-03 12:25:47 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Mundo Militar",
-  description: "...",
+  description: "Perri-ortitorico",
   icons: "@"
 };
 
