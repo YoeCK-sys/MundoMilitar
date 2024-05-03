2f4fd4509002e2e8a4cbe0c2e58666a1c51bf810
unknown <darkyoel75@gmail.com>
2024-05-03 10:47:48 -0300

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+import Image from "next/image";
+import { YoelEmpire } from "@/components/component/yoel-empire";
+
+export default function Home() {
+  return (
+   <YoelEmpire></YoelEmpire>
+  );
+}
