f0c87c1d8bac3b09e8e23d03b6df73360e97d377
YoeCK-sys <114443942+YoeCK-sys@users.noreply.github.com>
2024-05-04 18:53:06 -0300

    Update yoel-empire.tsx

--- components/component/yoel-empire.tsx
+++ components/component/yoel-empire.tsx
@@ -1,8 +1,4 @@
-/**
- * v0 by Vercel.
- * @see https://v0.dev/t/2y0goDjC8IU
- * Documentation: https://v0.dev/docs#integrating-generated-code-into-your-nextjs-app
- */
+
 import Link from "next/link"
 
 export default function YoelEmpire() {
